e9ebc42433c72ea8b1f2860f7af5d11f7639b1eb
Nikita Kuzhovnik <remark.kuzhovnik@gmail.com>
2022-06-06 21:15:25 +0300

    add dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Layout } from './shared/Layout';
 import { Header } from './shared/Header';
 import { Content } from './shared/Content';
 import { CardsList } from './shared/CardsList';
+import { Dropdown } from './shared/Dropdown';
 
 function AppComponent(): JSX.Element {
   return (
@@ -12,6 +13,12 @@ function AppComponent(): JSX.Element {
       <Header />
       <Content>
         <CardsList />
+        <br />
+        <Dropdown onClose={() => console.log('closed')} onOpen={() => console.log('open')} button={<button>test</button>}>
+          <ul>
+            <li>a</li>
+          </ul>
+        </Dropdown>
       </Content>
     </Layout>
   );
